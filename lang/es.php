6818240d4fd25d315054b989aec4518f210518c2
Tecnoestrategia <tecnoestrategia@users.noreply.github.com>
2017-02-27 22:27:04 +0100

    Update es.php

--- lang/es.php
+++ lang/es.php
@@ -7,7 +7,7 @@ define("BACK_TO_TOP", "Subir");
 define("PRIVACY", "Privacidad");
 define("TERMS", "Terminos de uso");
 define("COMPANY", "Empresa");
-define("PROJECT_NAME", "Ejemplo de MVC en PHP, con PDO, CRUD y Bootstrap");
+define("PROJECT_NAME", "Ejemplo de MVC en PHP, con PDO y Bootstrap");
 define("MENU_HOME", "Inicio");
 define("MENU_ABOUT", "Que es esto");
 define("MENU_CONTACT", "Contacto");
