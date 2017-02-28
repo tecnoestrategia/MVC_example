3d87e6d591f56b62910c77694464cdfbcbec9312
Tecnoestrategia <tecnoestrategia@users.noreply.github.com>
2017-03-01 00:48:00 +0100

    Update date.php

--- functions/date.php
+++ functions/date.php
@@ -17,5 +17,18 @@ class DateFunctions {
 	public function actual_mysql_timestamp(){
 	return date('F/j/Y',strtotime($result['postDate']));
 	}
+	function weeks_in_month($month, $year){
+    		$startDate = new DateTime();
+    		$startDate->setDate($year, $month, 1);
+		$loopDate = $startDate;
+    		$week = 1;
+    			for($i = $startDate->format('d'); $i <= cal_days_in_month(CAL_GREGORIAN, $month, $year); $i++){
+        			if($loopDate->format('w') % 7 == 0){
+					$week++;
+				}
+				$loopDate->modify('+1 day');
+			}
+    	return $week;
+	}
 }
 ;?>
