ea9b20f0f138ca0e6d358cbcaa7cc5e34d98d852
Tecnoestrategia <tecnoestrategia@users.noreply.github.com>
2017-03-06 14:58:16 +0100

    Add files via upload

---
+++ lang/en.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Constant to EN english language
+*/
+/**
+* @author TecnoEstrategia <develop@tecnoestrategia.com>
+* @copyright TecnoEstrategia
+* @license GPL
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link https://github.com/tecnoestrategia This source code
+* @todo write a little example of template engine
+*/
+define("TITLE", "Title");
+define("NAME", "Name");
+define("MESSAGE", "Message");
+define("LIST", "List");
+define("BACK_TO_TOP", "Up");
+define("PRIVACY", "Privacity");
+define("TERMS", "Terms of use");
+define("COMPANY", "Company");
+define("PROJECT_NAME", "Example of MVC pattern with PDO  and Bootstrap");
+define("MENU_HOME", "Home");
+define("MENU_ABOUT", "About");
+define("MENU_CONTACT", "Contact");
+define("MENU_EXTRAS", "Extra");
+define("MENU_EXTRAS_MODEL", "Model");
+define("MENU_EXTRAS_VIEW", "View");
+define("MENU_EXTRAS_CONTROLLER", "Controler");
+?>
